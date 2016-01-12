e98092e8e06902460d56e8eba6d54b5e03f97801
Kiku <kiku.jones@quinnipiac.edu>
2016-01-12 17:32:34 +0000

    Add Question create view

--- core/models.py
+++ core/models.py
@@ -3,3 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    title = models.CharField(max_length=300)
+    description = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
